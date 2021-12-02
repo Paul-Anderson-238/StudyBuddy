51a5eb7bb50aa8afd991e309774945868d80339d
Helix088 <pat20001@byui.edu>
2021-12-01 22:19:21 -0700

    Added Intent and Shared Preference

--- app/src/main/java/com/example/studybuddy/EditCardMenu.java
+++ app/src/main/java/com/example/studybuddy/EditCardMenu.java
@@ -3,12 +3,18 @@ package com.example.studybuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.gson.Gson;
+
+import java.io.Serializable;
 import java.util.List;
 
 public class EditCardMenu extends AppCompatActivity {
     List<String> setList;
+    public String setName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,20 @@ public class EditCardMenu extends AppCompatActivity {
 
         Intent intent = getIntent();
         setList = (List<String>) intent.getSerializableExtra("SETLIST");
+    }
+
+    //Create shared preferences for the name of lists
+    public void listName(View view) {
+       SharedPreferences sharedPreferences = getSharedPreferences("SHAREDPREF", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String string = sharedPreferences.getString(setName, "setname");
+        setList = gson.fromJson(string, List.class);
+    }
+
+    //Create Intent to connect pass set list to edit card view. See main, pass the set name we are trying to edit.
+    public void setName(View view){
+        Intent intent = new Intent(this, EditCardMenu.class);
+        intent.putExtra("setname", (Serializable) setName);
+        startActivity(intent);
     }
 }

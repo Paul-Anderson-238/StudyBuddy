51d11a50cb769994804d5bc83d468776fe45d69e
Rex-1nvictus <and13005@byui.edu>
2021-12-02 19:50:18 -0700

    Added Create New Set to EditCardMenu

--- app/src/main/java/com/example/studybuddy/EditCardMenu.java
+++ app/src/main/java/com/example/studybuddy/EditCardMenu.java
@@ -1,11 +1,14 @@
 package com.example.studybuddy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 
@@ -15,6 +18,12 @@ import java.util.List;
 public class EditCardMenu extends AppCompatActivity {
     List<String> setList;
     public String setName;
+
+    //data for creating a popup screen
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText newSetName;
+    private Button saveNewSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +43,31 @@ public class EditCardMenu extends AppCompatActivity {
     }
 
     //Create Intent to connect pass set list to edit card view. See main, pass the set name we are trying to edit.
-    public void setName(View view){
+    public void editSet(View view){
         Intent intent = new Intent(this, EditCardMenu.class);
         intent.putExtra("setname", (Serializable) setName);
         startActivity(intent);
+    }
+
+    //Add a New Set
+    public void newSet(View view) {
+        Intent intent = new Intent(this, EditCardMenu.class);
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View newSetPopupView = getLayoutInflater().inflate(R.layout.newsetpopup, null);
+        newSetName = (EditText) findViewById(R.id.newSetName);
+        saveNewSet = (Button) findViewById(R.id.saveNewSet);
+
+        dialogBuilder.setView(newSetPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+
+        saveNewSet.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText editor = (EditText) findViewById(R.id.newSetName);
+                setName = editor.getText().toString();
+            }
+        });
     }
 }

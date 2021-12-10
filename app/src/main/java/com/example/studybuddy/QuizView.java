b9180efd1893925bd8e12cbd3498c739a07bbb42
smithbh1 <smithbh129@gmail.com>
2021-12-09 18:44:32 -0700

    started working/making nextCard, removeCard, and flipCard onClick functions

--- app/src/main/java/com/example/studybuddy/QuizView.java
+++ app/src/main/java/com/example/studybuddy/QuizView.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -12,6 +13,7 @@ public class QuizView extends AppCompatActivity {
     CardSet set;
     Card currentCard;
     int currentCardIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +27,23 @@ public class QuizView extends AppCompatActivity {
         //Make sure the set has at least 1 card and display the front
         if (set.getSize() > 0){
             Card front = set.getCard(currentCardIndex);
-            TextView text = (TextView) findViewById(R.id.cardFront);
-            text.setText(currentCardIndex+1);
+            TextView text = (TextView) findViewById(R.id.cFront);
+            text.setText(currentCardIndex);
         }
 
     }
     public void nextCard(View view){
-
+        currentCard = set.getCard(currentCardIndex);
+        displayCard();
     }
     public void removeCard(View view){
+        //set.remove(currentCardIndex);
+    }
+    void displayCard() {
+        EditText e = findViewById(R.id.cardFront);
+        e.setText(currentCard.getFront());
+    }
+    public void flipCard(){
 
     }
 

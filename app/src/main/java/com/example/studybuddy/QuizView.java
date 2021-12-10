3d71ef9a42df78ff68b9bd9c5b69a89d8e94e9b0
Rex-1nvictus <and13005@byui.edu>
2021-12-10 15:31:26 -0700

    The onCreate Function now works properly

--- app/src/main/java/com/example/studybuddy/QuizView.java
+++ app/src/main/java/com/example/studybuddy/QuizView.java
@@ -6,32 +6,83 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class QuizView extends AppCompatActivity {
     CardSet set;
     Card currentCard;
     int currentCardIndex;
-
+    String filename;
+    String setName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_view);
 
-//Name of Set we'll need from the incoming intent to read each card in the list
+        //Name of Set we'll need from the incoming intent to read each card in the list
         Intent intent = getIntent();
-        Gson gson = new Gson();
+        setName = intent.getStringExtra("setname");
 
-        //Make sure the set has at least 1 card and display the front
-        if (set.getSize() > 0){
-            Card front = set.getCard(currentCardIndex);
-            TextView text = (TextView) findViewById(R.id.numberOfCards);
-            text.setText((currentCardIndex + 1) + "/" + (set.getSize()));
+        //read the File
+        readFile();
+
+        //Check to make sure there is at least one card in the set
+        if(set.getSize() == 0){
+            intent = new Intent(this, QuizMenu.class);
+            Toast.makeText(this, "ERROR: MUST HAVE AT LEAST ONE CARD IN SET", Toast.LENGTH_LONG).show();
+            startActivity(intent);
         }
 
+        //Shuffle the cards and set the display to the new first card
+        set.randomizeCards();
+        currentCard = set.getCard(0);
+        displayCard();
     }
+
+    public void readFile(){
+        //A few preliminary set up variables
+        filename = setName + ".txt";
+        Gson gson = new Gson();
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(filename);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String inputFromFile;
+
+            while((inputFromFile = br.readLine()) != null){
+                sb.append(inputFromFile).append("\n");
+            }
+
+            set = gson.fromJson(sb.toString(), CardSet.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //If the set doesn't currently exist
+        if(set == null)
+            set = new CardSet(setName);
+    }
+
     public void nextCard(View view){
         if(currentCardIndex == set.getSize()-1)
             currentCardIndex = 0;
@@ -45,7 +96,7 @@ public class QuizView extends AppCompatActivity {
         //set.remove(currentCardIndex);
     }
     void displayCard() {
-        EditText e = findViewById(R.id.cardFront);
+        TextView e = findViewById(R.id.cFront);
         e.setText(currentCard.getFront());
     }
     public void flipCard(){
